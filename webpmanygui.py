a7d4e0bf05de9e10ad7fdccf7fd7b93d31a13000
William Burnham <burnham.william@gmail.com>
2021-04-10 18:47:57 +0200

    add webp gui

--- webpmanygui.py
+++ webpmanygui.py
@@ -3,7 +3,7 @@ from tkinter import filedialog
 from tkinter import messagebox
 import os
 
-from imgoptimizr.imageutil.utils import thumbnails_exact
+from imgoptimizr.imageutil.utils import webps
 
 
 def int_val(string):
@@ -13,13 +13,13 @@ def int_val(string):
         return -1
 
 
-class ThumbnailExactManyGui:
+class WebPManyGui:
     def __init__(self):
         unit = 15
         row = 1
         self.root = tk.Tk()
-        self.root.geometry('200x380')
-        self.root.title('Thumbnail Exact Many')
+        self.root.geometry('200x215')
+        self.root.title('WebP Many')
         self.src_label = tk.Label(self.root, text='Source directory').place(x=unit, y=unit * row)
         row += 2
         self.src_dir_val = tk.StringVar()
@@ -36,19 +36,7 @@ class ThumbnailExactManyGui:
         self.dest_button = tk.Button(self.root, text='...', command=self.choose_dest)
         self.dest_button.place(x=unit * 10, y=unit * row)
         row += 3
-        self.width_label = tk.Label(self.root, text='Width').place(x=unit, y=unit * row)
-        row += 2
-        self.width_val = tk.StringVar()
-        self.width = tk.Entry(self.root, textvariable=self.width_val)
-        self.width.place(x=unit, y=unit * row)
-        row += 3
-        self.height_label = tk.Label(self.root, text='Height').place(x=unit, y=unit * row)
-        row += 2
-        self.height_val = tk.StringVar()
-        self.height = tk.Entry(self.root, textvariable=self.height_val)
-        self.height.place(x=unit, y=unit * row)
-        row += 3
-        self.create_button = tk.Button(self.root, text='Create', command=self.create_thumbnail_exact_many)
+        self.create_button = tk.Button(self.root, text='Create', command=self.create_webp_many)
         self.create_button.place(x=unit, y=unit * row)
 
     def thumbnail_exact_many_window(self):
@@ -62,22 +50,18 @@ class ThumbnailExactManyGui:
         result = filedialog.askdirectory()
         self.dest_dir_val.set(result)
 
-    def create_thumbnail_exact_many(self):
+    def create_webp_many(self):
         src_dir = self.src_dir_val.get()
         dest_dir = self.dest_dir_val.get()
         if not os.path.isdir(src_dir):
             messagebox.showerror('Invalid directory', 'Source directory not valid')
             return
         # todo check if dest path is valid path without creating it if it doesn't exist
-        width = int_val(self.width_val.get())
-        height = int_val(self.height_val.get())
-        if width < 0 or height < 0:
-            messagebox.showerror('Invalid dimensions', 'Width / height not valid')
         try:
-            thumbnails_exact(src_dir, dest_dir, (width, height))
-            messagebox.showinfo('Success', 'Finished creating thumbnails')
+            webps(src_dir, dest_dir)
+            messagebox.showinfo('Success', 'Finished creating webp files')
         except BaseException as error:
             messagebox.showerror('Error', error)
 
 
-ThumbnailExactManyGui().root.mainloop()
+WebPManyGui().root.mainloop()
